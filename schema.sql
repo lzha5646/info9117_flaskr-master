cdd9fd2c45896b086f1fdb59a5954a5a03e23b44
Lei Zheng <lzha5646@uni.sydney.edu.au>
2015-03-21 18:23:14 +1100

    database error

---
+++ schema.sql
@@ -0,0 +1,11 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+
+  title text not null,
+  text text not null,
+  username text not null,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
+);
+
