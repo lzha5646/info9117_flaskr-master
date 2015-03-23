3f4e1d82dbb22db3f9c86cfd5dbb462c9a446fe3
Lei Zheng <lzha5646@uni.sydney.edu.au>
2015-03-23 21:34:43 +1100

    all good one

--- flaskr.py
+++ flaskr.py
@@ -48,7 +48,6 @@ def add_entry():
         abort(401)
     g.db.execute('insert into entries (title, text, username, start_time, end_time) values (?,?,?,?,?)',
                  [request.form['title'], request.form['text'], session['username'], request.form['start_time'], request.form['end_time']])
-
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
